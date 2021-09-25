aa692f885312aabba0775fa34e2c7d68ce597c2a
Kim JinYeon <hourglass458@gmail.com>
2021-09-25 22:27:24 +0900

    coronaboard update

---
+++ 10_team_project/coronaboard.py
@@ -0,0 +1,69 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
+import datetime
+import pandas as pd
+import re
+
+# 크롬 창을 띄우지 않고 실행하는 옵션
+chrome_option = webdriver.ChromeOptions()
+# chrome_option.add_argument('headless')
+chrome_option.add_argument("disable-gpu")
+
+# 셀레니움 웹드라이버 실행
+driver = webdriver.Chrome('chromedriver', options=chrome_option)
+driver.implicitly_wait(5)
+
+# 코로나보드 url
+url_corona = 'https://coronaboard.kr/'
+
+# 웹드라이버 실행
+driver.get(url_corona)
+driver.implicitly_wait(5)
+time.sleep(10)
+
+# 페이지 소스 받아와서 bs로 parse
+page = driver.page_source
+soup = BeautifulSoup(page, 'html.parser')
+driver.implicitly_wait(5)
+
+# 정보 담을 list 생성
+
+information = [[] for i in range(11)]
+
+# 코로나 정보 테이블 찾기
+country_table = soup.find('div', id='country-table').find('tbody')
+
+# 나라 리스트
+country_list = country_table.find_all('tr')
+
+for country in country_list:
+    info_list = country.find_all('td')
+    index = 0
+    for info in info_list:
+        text = info.text
+        text = text.replace(',', '')
+        text = re.sub('[(].*[)]', '', text)
+        information[index].append(text)
+        index += 1
+
+# 오늘 날짜
+now = datetime.datetime.now()
+now_date = now.strftime('%Y-%m-%d')
+
+# 파일명 정하기
+file_name = f'coronaboard({now_date}).csv'
+
+dict_info = {'국가':information[1],
+             '확진자':information[2],
+             '치료중':information[3],
+             '위중증':information[4],
+             '사망자':information[5],
+             '완치':information[6],
+             '치명(%)':information[7],
+             '완치(%)':information[8],
+             '발생률':information[9],
+             '인구수':information[10]}
+
+corona_info = pd.DataFrame(dict_info)
+corona_info.to_csv(file_name, index=False, encoding='utf-8-sig')
